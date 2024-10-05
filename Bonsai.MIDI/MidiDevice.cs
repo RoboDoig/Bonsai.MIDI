9dfd21544f5d72dfb2a4c9b9bccb1227f7f04c95
RoboDoig <erskine94@gmail.com>
2024-10-05 13:27:34 -0400

    Basic control event reader

---
+++ Bonsai.MIDI/MidiDevice.cs
@@ -0,0 +1,44 @@
+﻿using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Multimedia;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.MIDI
+{
+    public class MidiDevice : Source<ControlChangeEvent>
+    {
+        [TypeConverter(typeof(MidiDeviceNameConverter))]
+        public string DeviceName { get; set; }
+
+        public override IObservable<ControlChangeEvent> Generate()
+        {
+            return Observable.Create<ControlChangeEvent>(async observer =>
+            {
+                var device = await Task.Run(() => InputDevice.GetByName(DeviceName));
+
+                EventHandler<MidiEventReceivedEventArgs> inputReceived = (sender, e) =>
+                {
+                    ControlChangeEvent changeEvent = (ControlChangeEvent)e.Event;
+
+                    observer.OnNext(changeEvent);
+                };
+
+                device.EventReceived += inputReceived;
+                device.StartEventsListening();
+
+                return Disposable.Create(() =>
+                {
+                    device.StopEventsListening();
+                    device.EventReceived -= inputReceived;
+                    device.Dispose();
+                });
+            });
+        }
+    }
+}
---
+++ Bonsai.MIDI/MidiDeviceNameConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Multimedia;
+
+namespace Bonsai.MIDI
+{
+    public class MidiDeviceNameConverter : StringConverter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            return new StandardValuesCollection(InputDevice.GetAll().Select(x => x.Name).ToList());
+        }
+    }
+}
